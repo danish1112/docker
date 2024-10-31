e43b3fb727797898a4f0c91c216ca831a07c2b97
Danish Yousuf Mir <danishyousufmir@Danishs-Laptop.local>
2024-10-31 19:11:25 +0530

    docker express redis

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.send('<h1>response is from docker</h1>')
+})
+
+app.listen(port, () => {
+    console.log(`server is running on port ${port}`)
+})
