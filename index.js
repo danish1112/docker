d61353b6b12345fff3c74e70396b63249cb6a962
Danish Yousuf Mir <danishmir9991@gmail.com>
2024-11-02 19:13:55 +0530

    added config in docker file

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 const express = require("express");
 
+const mongoose = require("mongoose");
+const { MONGO_USER, MONGO_PASSWORD, MONGO_IP, MONGO_PORT } = require("./config");
+
 const app = express();
 
 const port = process.env.PORT || 3000;
 
+const MONGO_URL = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}`
+
+mongoose.connect(MONGO_URL).then(() => {
+    console.log("mongo connected successfully");
+}).catch((err) => {
+    console.log("error in connecting mongodb", err);
+})
+
 app.get('/', (req, res) => {
-    res.send('<h1>response is from docker!!!</h1>')
+    res.send('<h1>response is from docker</h1>')
 })
 
 app.listen(port, () => {
